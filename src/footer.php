2eb90d37959419a4dc0c1eaca4d3e3952a7c9c45
Emmanuel SCELLES <escelles@gmail.com>
2021-03-26 10:42:11 +0100

    reproduction de netflix en php

---
+++ src/footer.php
@@ -0,0 +1,9 @@
+<footer>
+	<div class="container">
+		<p>Des questions ? Appelez THANOS au 0800 917 813 (50€ /minute)</p>
+		<a href="#">Conditions des cartes cadeaux</a>
+		<a href="#">Conditions d'utilisation</a>
+		<a href="#">Déclaration de confidentialité</a>
+		<a href="#"> 2021 &copy; Thanos </a>
+	</div>
+</footer>
